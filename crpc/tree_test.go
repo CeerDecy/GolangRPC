b48af3a51e82977299b3d31864d219365299f93f
CeerDecy <1748788674@qq.com>
2023-06-16 14:41:42 +0800

    tree path

---
+++ crpc/tree_test.go
@@ -0,0 +1,18 @@
+package crpc
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestName(t *testing.T) {
+	root := &treeNode{name: "/"}
+	root.Put("/abc/test")
+	root.Put("/abc/ttt")
+	root.Put("/def/**")
+	root.Put("/gh/:id")
+	fmt.Println(root.Get("/abc/ttt"))
+	fmt.Println(root.Get("/abc/sss"))
+	fmt.Println(root.Get("/def/3443/fudn"))
+	fmt.Println(root.Get("/gh/3443"))
+}
